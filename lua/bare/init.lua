c311d04d36468e7ce75fe8b869a18ccc604208eb
Himanshu <manshu3220@gmail.com>
2025-10-04 17:05:24 +0530

    fix init.lua

--- lua/bare/init.lua
+++ lua/bare/init.lua
@@ -1,10 +1,8 @@
-local modules = { "status", "buffer", "icons", "snippets", "liveserver" }
+local modules = { "buffer", "icons", "liveserver", "snippets", "status" }
 
 for _, mod in ipairs(modules) do
-    local ok, plugin = pcall(require, "bare." .. mod)
-    if ok and type(plugin) == "table" and plugin.setup then
-        plugin.setup()
-    elseif not ok then
+    local ok, _ = pcall(require, "bare." .. mod)
+    if not ok then
         vim.notify("Failed to load bare.nvim module: " .. mod, vim.log.levels.WARN)
     end
 end
