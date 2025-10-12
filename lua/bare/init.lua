da75448c6bf4ac40c3ac1867147fbe34015fcd3e
Himanshu <manshu3220@gmail.com>
2025-10-12 14:53:38 +0530

    fix icons

--- lua/bare/init.lua
+++ lua/bare/init.lua
@@ -2,13 +2,23 @@
 require("bare.onedark")
 require("bare.buffer")
 require("bare.status")
-require("bare.netrw")
-require("bare.lsp")
 require("bare.cmp")
 require("bare.fzf").setup()
-require("bare.surround").setup()
-require("bare.marks").setup()
+require("bare.netrw")
 require("bare.filetree").setup()
-require("bare.pairs").setup()
-require("bare.liveserver").setup()
 
+vim.defer_fn(function()
+  require("bare.liveserver").setup()
+  require("bare.marks").setup()
+  require("bare.surround").setup()
+  require("bare.pairs").setup()
+end, 50)
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  once = true,
+  callback = function()
+    require("bare.lsp")
+  end,
+})
