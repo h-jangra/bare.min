237674f93c3eb9b99a0577831ed0f65fa95ebd7f
Himanshu <manshu3220@gmail.com>
2026-05-09 11:58:26 +0530

    update lsp

--- lua/bare/lsp.lua
+++ lua/bare/lsp.lua
@@ -11,27 +11,7 @@ local servers = {
   taplo = { cmd = { "taplo", "lsp", "stdio" }, ft = { "toml" } },
   bash_lsp = { cmd = { "bash-language-server", "start" }, ft = { "bash", "sh" } },
   tinymist = { cmd = { "tinymist", "lsp" }, ft = { "typst" }, settings = { exportPdf = 'onType', formatterMode = 'typstyle' } },
-  jdtls = {
-    cmd = {
-      "java",
-      "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-      "-Dosgi.bundles.defaultStartLevel=4",
-      "-Declipse.product=org.eclipse.jdt.ls.core.product",
-      "-Dlog.protocol=true",
-      "-Dlog.level=ALL",
-      "-Xmx1g",
-      "--add-modules=ALL-SYSTEM",
-      "--add-opens", "java.base/java.util=ALL-UNNAMED",
-      "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-
-      "-jar", vim.fn.glob("~/.local/share/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-
-      "-configuration", "~/.local/share/jdtls/config_linux",
-
-      "-data", vim.fn.stdpath("data") .. "/jdtls-workspace"
-    },
-    ft = { "java" },
-  },
+  jdtls = { cmd = { "jdtls", }, ft = { "java" }, },
   tailwindcss = {
     cmd = { "tailwindcss-language-server", "--stdio" },
     ft = {
